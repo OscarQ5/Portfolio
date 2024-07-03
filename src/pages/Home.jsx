e5abf8b7f40b5c3cefba92195f7a54f0220c939d
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2024-07-03 02:12:06 -0400

    initialized new clean vite project w/ basic home page, also installed needed dependencies including react dom & bulma

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio</h1>
+            <p>Hi, I'm Oscar Quintanilla, a passionate full-stack developer looking to break into the tech world.</p>
+        </div>
+    );
+};
+
+export default Home;
