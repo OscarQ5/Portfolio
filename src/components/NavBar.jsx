6386dead59612210bf11abb9668c9ab758ef7a3b
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2024-07-03 19:06:16 -0400

    removed bulma css due to default dark mode and will use another framework

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <Link className="navbar-item" to="/">Home</Link>
+        <nav>
+            <div>
+                <Link to="/">Home</Link>
             </div>
 
-            <div className="navbar-menu">
-                <div className="navbar-start">
-                    <Link className="navbar-item" to="/resume">Resume</Link>
-                    <Link className="navbar-item" to="/projects">Projects</Link>
+            <div>
+                <div >
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/projects">Projects</Link>
                 </div>
             </div>
         </nav>
